0f495fba16e8d1e0a1a1232ad3f057598c2feb92
kikiprinci <farboleya@gmail.com>
2019-11-11 12:19:06 +0100

    add 2 new private methods to simplify PlayView public method

--- draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/PlayView.java
+++ draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/PlayView.java
@@ -6,27 +6,37 @@ import es.urjccode.mastercloudapps.adcs.draughts.models.Coordinate;
 
 public class PlayView extends WithConsoleView {
 
+    private GameView gameView;
+
     public PlayView() {
         super();
+        gameView = new GameView();
     }
 
     public void interact(PlayController playController) {
         String color = MessageView.COLORS[playController.getColor().ordinal()];
         Error error = null;
-        GameView gameView = new GameView();
         do {
 
             String command = this.console
                     .readString(MessageView.MOVE.getMessage() + color + MessageView.COLON.getMessage());
             error = playController.move(new Coordinate(command.substring(0, 2)),
                     new Coordinate(command.substring(3, 5)));
-            if (error != null) {
-                console.writeln(MessageView.ERROR.getMessage() + error.name());
-                gameView.write(playController);
-            }
+            checkEndGame(playController, error);
         } while (error != null);
+        checkIsBlocked(playController);
+    }
+
+    private void checkIsBlocked(PlayController playController) {
         if (playController.isBlocked()) {
             this.console.write(MessageView.MESSAGEPLAY.getMessage());
+        }
+    }
+
+    private void checkEndGame(PlayController playController, Error error) {
+        if (error != null) {
+            console.writeln(MessageView.ERROR.getMessage() + error.name());
+            gameView.write(playController);
         }
     }
 
